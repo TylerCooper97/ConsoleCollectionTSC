cfe2700aa8923c38ad6abd3735fa1e4eaf78cc8a
tcooper <tcooper@localhost>
2016-05-02 18:36:06 -0400

    added instance variables and methods

---
+++ ConsoleCollectionTSC/src/edu/saintjoe/cs/tcooper/consoledriver/Consoles.java
@@ -0,0 +1,34 @@
+package edu.saintjoe.cs.tcooper.consoledriver;
+
+public class Consoles {
+
+	private String name;
+	private String age;
+	
+	
+	public void setName(String newName){
+		name = newName;
+	}
+	public String getName(){
+		return name;
+	}
+	
+	public void setAge(String newAge){
+		age = newAge;
+	}
+	public String getAge(){
+		return age;
+	}
+	
+	public Consoles(String newName, String newAge){
+		name = newName;
+		age = newAge;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
